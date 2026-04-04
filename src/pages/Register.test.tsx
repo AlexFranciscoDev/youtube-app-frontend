0607bb366944c683e4b45a2179c2bcc08b219ed8
AlexFranciscoDev <alexbcn98gmail.com>
2026-04-05 00:57:48 +0200

    test: username is valid

--- src/pages/Register.test.tsx
+++ src/pages/Register.test.tsx
@@ -2,6 +2,7 @@ import { Register } from "./Register";
 import { fireEvent, render, screen } from "@testing-library/react";
 import { MemoryRouter } from "react-router-dom";
 import { AuthProvider } from "../context/AuthProvider";
+import userEvent from "@testing-library/user-event";
 
 const navigateMock = vi.fn();
 
@@ -39,8 +40,8 @@ describe("Register component", () => {
     const password = screen.getByLabelText("Password");
     const confirmPassword = screen.getByLabelText("Confirm password");
     const profileImage = screen.getByLabelText("Profile image");
-    const button = screen.getByRole("button", {name: /Create account/i});
-    const signInLink = screen.getByRole("link", {name: /Sign in/i});
+    const button = screen.getByRole("button", { name: /Create account/i });
+    const signInLink = screen.getByRole("link", { name: /Sign in/i });
 
     expect(title).toBeInTheDocument();
     expect(text).toBeInTheDocument();
@@ -51,5 +52,50 @@ describe("Register component", () => {
     expect(profileImage).toBeInTheDocument();
     expect(button).toBeInTheDocument();
     expect(signInLink).toBeInTheDocument();
+  });
+
+  describe("Username validation", () => {
+    it("Username is required", async () => {
+      const username = screen.getByLabelText("Username");
+      fireEvent.blur(username);
+      const errorRequired = screen.getByText("Username is required");
+      expect(errorRequired).toBeInTheDocument();
+    });
+
+    it("Username must be at least 5 characters long", async () => {
+      const user = userEvent.setup();
+      const username = screen.getByLabelText("Username");
+      await user.type(username, "alex");
+      fireEvent.blur(username);
+      const errorLength = screen.getByText(
+        "Username must be at least 5 characters",
+      );
+      expect(errorLength).toBeInTheDocument();
+    });
+
+    it("Username must not contain spaces", async () => {
+      const user = userEvent.setup();
+      const username = screen.getByLabelText("Username");
+      await user.type(username, "alex dev");
+      fireEvent.blur(username);
+      const errorSpaces = screen.getByText("Username cannot contain spaces");
+      expect(errorSpaces).toBeInTheDocument();
+    });
+
+    it("Valid username", async () => {
+      const user = userEvent.setup();
+      const username = screen.getByLabelText("Username");
+      await user.type(username, "alexdev");
+      fireEvent.blur(username);
+      expect(
+        screen.queryByText("Username is required"),
+      ).not.toBeInTheDocument();
+      expect(
+        screen.queryByText("Username must be at least 5 characters"),
+      ).not.toBeInTheDocument();
+      expect(
+        screen.queryByText("Username cannot contain spaces"),
+      ).not.toBeInTheDocument();
+    });
   });
 });
