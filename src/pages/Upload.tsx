511bbffd5213cd6f24d64b0cdc386e3c3d4b89ab
AlexFranciscoDev <alexbcn98gmail.com>
2026-04-29 23:41:55 +0200

    feat: upload video route

---
+++ src/pages/Upload.tsx
@@ -0,0 +1,5 @@
+export const Upload = () => {
+    return (
+        <h1>Upload video</h1>
+    )
+}
--- src/router/Routing.tsx
+++ src/router/Routing.tsx
@@ -4,6 +4,7 @@ import { Login } from '../pages/Login.tsx';
 import { Register } from '../pages/Register.tsx';
 import { NotFound } from '../pages/NotFound.tsx';
 import { MainLayout } from "../layouts/MainLayout";
+import { Upload } from '../pages/Upload.tsx';
 import ProtectedRoute from '../helpers/ProtectedRoute';
 
 export const Routing = () => {
@@ -12,6 +13,7 @@ export const Routing = () => {
             <Route path="/" element={<MainLayout />}>
                 <Route element={<ProtectedRoute />}>
                     <Route index element={<Home />} />
+                    <Route path='/upload' element={<Upload />} />
                 </Route>
                 <Route path='/register' element={<Register />} />
                 <Route path='/login' element={<Login />} />
