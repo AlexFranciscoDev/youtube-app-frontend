723e1c72167bfbdaf629da675485c78db5ea6c50
AlexFranciscoDev <alexbcn98gmail.com>
2026-05-31 01:29:14 +0200

    fix: claude.md updated and logo link to homepage

--- src/layouts/Header.tsx
+++ src/layouts/Header.tsx
@@ -11,7 +11,7 @@ export const Header = () => {
         <header className='header'>
             <div className=" layout-container mx-auto flex flex-col justify-between items-center p-5 sm:flex-row">
                 <div className="header-content flex flex-col gap-2">
-                    <h1 className='text-3xl font-bold'>Video Organizer</h1>
+                    <h1 className='text-3xl font-bold'><Link to='/'>Video Organizer</Link></h1>
                     <span className=''>Organize and manage your videos</span>
                 </div>
                 <nav className="nav pt-3 md:pt-0">
